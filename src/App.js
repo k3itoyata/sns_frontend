1b9fc78344a5a9d16496b196beaa9f83b129001e
矢田桂都 <yatakeito@makkubukkuea.local>
2022-09-21 11:59:35 +0900

    firsttime

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import './App.css';
+import { createTheme } from "@material-ui/core/styles";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import { indigo } from "@material-ui/core/colors";
+import Navbar from "./components/Navbar";
+import ApiContextProvider from "./context/ApiContext";
+import Main from "./components/Main";
+
+
+
+const theme = createTheme({
+  palette: {
+    primary: indigo,
+    secondary: {
+      main: "#f44336",
+    },
+  },
+  typography: {
+    fontFamily: "Comic Neue",
+  },
+});
+
+function App() {
+  return (
+    <ApiContextProvider>
+      <MuiThemeProvider theme={theme}>
+        <Navbar />
+        <div className="container">
+          <Main />
+        </div>
+      </MuiThemeProvider>
+    </ApiContextProvider>
+  );
+}
+
+export default App;
